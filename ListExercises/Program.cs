672593f44d0591288ac253532f19f38b8da0fdfb
Peter Peev <peev.petar@gmail.com>
2019-11-21 00:38:13 +0200

    Add project files.

---
+++ ListExercises/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ListExercises
+{
+using System;
+
+
+class Program
+	{
+		static void Main()
+		{
+			var text = "pesho gosho i ivan";
+
+			var count = new int[26];
+
+			foreach (var symbol in text)
+			{
+				if (char.IsLetter(symbol))
+				{
+					count[symbol - 'a']++;
+				}
+			}
+
+			for (int i = 0; i < count.Length; i++)
+			{
+				if (count[i]>0)
+				{
+					Console.WriteLine($"{(char)(i +'a')} - {count[i]}");
+				}
+			}
+		}
+	}
+}
